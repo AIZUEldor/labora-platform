f30e63f4f3c902ce877e5b23eeb7f57316952fe6
eldorroziyev0356@gmail.com <eldorroziyev0356@gmail.com>
2026-05-18 07:59:16 +0500

    feat: add user repository

---
+++ Labora.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using Labora.Domain.Entities;
+using Labora.Domain.Interfaces;
+
+namespace Labora.Domain.Interfaces;
+
+public interface IUserRepository : IGenericRepository<User>
+{
+    Task<User?> GetByPhoneNumberAsync(string phoneNumber);
+    Task<bool> PhoneNumberExistsAsync(string phoneNumber);
+}
---
+++ Labora.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,28 @@
+﻿using Labora.Domain.Entities;
+using Labora.Domain.Interfaces;
+using Labora.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Labora.Infrastructure.Repositories;
+
+public class UserRepository : GenericRepository<User>, IUserRepository
+{
+    private readonly LaboaDbContext _context;
+
+    public UserRepository(LaboaDbContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    public async Task<User?> GetByPhoneNumberAsync(string phoneNumber)
+    {
+        return await _context.Users
+            .FirstOrDefaultAsync(u => u.PhoneNumber == phoneNumber && !u.IsDeleted);
+    }
+
+    public async Task<bool> PhoneNumberExistsAsync(string phoneNumber)
+    {
+        return await _context.Users
+            .AnyAsync(u => u.PhoneNumber == phoneNumber && !u.IsDeleted);
+    }
+}
