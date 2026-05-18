881de39246780443adb2fce1bc2952830db9e48b
eldorroziyev0356@gmail.com <eldorroziyev0356@gmail.com>
2026-05-18 07:21:18 +0500

    feat: add LaboaDbContext with entity configurations

---
+++ Labora.Domain/Entities/Transaction.cs
@@ -0,0 +1,16 @@
+﻿using Labora.Domain.Common;
+
+namespace Labora.Domain.Entities;
+
+public class Transaction : BaseEntity
+{
+    public decimal Amount { get; set; }
+    public string Type { get; set; } = string.Empty; // "deposit" | "withdrawal"
+    public string? Description { get; set; }
+
+    // Foreign key
+    public Guid UserId { get; set; }
+
+    // Navigation property
+    public User User { get; set; } = null!;
+}
